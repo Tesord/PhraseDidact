c8f81d9f00b15155f1cc6977ff11975041a61c60
bennywan24 <truesword95@gmail.com>
2017-03-22 23:20:55 +0000

    BEFORE moving Register page

--- imports/Page/Entry/Register/registerForm.js
+++ imports/Page/Entry/Register/registerForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Accounts } from 'meteor/accounts-base';
 
 import Register_C from '../register';
 
@@ -31,10 +32,29 @@ class RegisterForm extends Component {
 	}
 
 
+	onSumbit(){
+		e.preventDefault();
+
+		let username = this.refs.username_field.value.trim();
+		let email = this.refs.email_field.value.trim();
+		let password = this.refs.pwd_field.value.trim();
+
+		Accounts.createUser({username, email, password}, (err) => {
+  			console.log('Signup callback', err);
+
+			// Client side will start to revert changes
+
+		});
+
+		// display loading UNTIL login method return ok status, then add user role
+	}
+
+
 	render(){
 
 		return (
-			<form id="register-form" className="_Theme_border_Default_ _Theme_register_Default_">
+			<form id="register-form" className="_Theme_border_Default_ _Theme_register_Default_"
+					onSumbit={	this.onSubmit.bind(this)	} >
 				<img src="img/ui/back-button-wfilled-gb.svg" width="40rem" height="40rem" className="form-corner-button" tabIndex="0"
 					onClick={ this.props.updateRegPage.bind(this, Register_C.Page_Enum.R_ENTRANCE )	}
 					ref={ (this_elem) => {this.back_button = this_elem;} }/>
@@ -63,14 +83,15 @@ class RegisterForm extends Component {
 
 				<loginAnnon className="h-center">	Username		</loginAnnon>
 				<input name="username" type="text" className="loginField		h-center		form-control"
-	   				 placeholder='e.g. david_smith72 '	/>
+	   				 placeholder='e.g. david_smith72 '		ref={	(this_elem) => (this.username_field = this_elem) }/>
 
 				<loginAnnon className="h-center">	Email Address		</loginAnnon>
 				<input name="email" type="email" className="loginField	h-center		form-control"
-						 placeholder='e.g. " david.smith@example.com  '	/>
+						 placeholder='e.g. " david.smith@example.com  '		ref={	(this_elem) => (this.email_field = this_elem) } />
 
 				<loginAnnon className="h-center">	Password		</loginAnnon>
-				<input name="password" type="password" className="loginField	h-center		form-control"/>
+				<input name="password" type="password" className="loginField	h-center		form-control"
+						 ref={	(this_elem) => (this.pwd_field = this_elem) } />
 
 				<loginAnnon className="h-center">	Confirm Password		</loginAnnon>
 				<input name="password2" type="password" className="loginField	h-center		form-control"/>
