8ddd12ea634ac53352d911cf4be538c54eeaf57e
bennywan24 <truesword95@gmail.com>
2017-04-04 00:13:54 +0000

    Up to speed

--- client/main.js
+++ client/main.js
@@ -3,23 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-<<<<<<< HEAD
-import Header_C from './Page/Common/header';
-
-import Home_C from './Page/home';
-import NotFound_C from './Page/notFound';
-import Entry_C from './Page/entry';
-import SetAccountType_C from './Page/setAccountType'
-
-/*************** TODO WHEN FINISHED: comment out the below imports & delete above.
- 					Move the 'Page' folder from 'client' to 'imports' **/
-
-// import Header_C from '/imports/Page/Common/header';
-
-// import Home_C from '/imports/Page/home';
-// import NotFound_C from '/imports/Page/notFound';
-// import Entry_C from '/imports/Page/entry';
-=======
 import Header__C from './ui/Common/header';
 
 import Home__C from './ui/home';
@@ -35,7 +18,6 @@ import FillLearnerProfile__C from './ui/LearnerExc/fillLearnerProfile';
 // import Home__C from '/imports/ui/home';
 // import NotFound__C from '/imports/ui/notFound';
 // import Entry__C from '/imports/ui/entry';
->>>>>>> da9362323b5ac475c5098d9317fa8c3794353bc2
 
 /****************** TODO ****************/
 
@@ -51,15 +33,6 @@ import FillLearnerProfile__C from './ui/LearnerExc/fillLearnerProfile';
 const routes = (
 	<Router>
 		<div>
-<<<<<<< HEAD
-			<Header_C />
-
-			<Switch>
-				<Route exact path="/" component={Home_C} />
-				<Route path="/login" component={Entry_C} />
-				<Route path="/selectAccountType" component={SetAccountType_C} />
-				<Route component={NotFound_C} />
-=======
 			<Header__C />
 
 			<Switch>
@@ -70,7 +43,6 @@ const routes = (
 				<Route path="/fillLearnerProfile" component={FillLearnerProfile__C} />
 
 				<Route component={NotFound__C} />
->>>>>>> da9362323b5ac475c5098d9317fa8c3794353bc2
 			</Switch>
 		</div>
 	</Router>
